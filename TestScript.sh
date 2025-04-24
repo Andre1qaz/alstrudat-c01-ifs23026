d206ece665a3976e80eff40b94a352793ee16997
Andre1qaz <andresaragih932@gmail.com>
2025-04-24 19:42:30 +0700

    try

--- TestScript.sh
+++ TestScript.sh
@@ -6,14 +6,14 @@ set +e
 # Compile Java program dengan Maven
 echo "🔨 Compiling Java program..."
 if ! mvn clean package; then
-  echo "✅ Build failed! Exiting..."
+  echo "❌ Build failed! Exiting..."
   exit 1
 fi
 echo "✅ Build successful!"
 
 # Pastikan file weights.txt ada
 if [ ! -f testcases/weights.txt ]; then
-  echo "✅ Error: File testcases/weights.txt not found!"
+  echo "❌ Error: File testcases/weights.txt not found!"
   exit 1
 fi
 
